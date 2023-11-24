55fb925cbba01103c9e4a98fe0fdd5d3e611ac21
Tawkir711 <mdtawkirk711@gmail.com>
2023-11-24 09:35:53 +0600

    Footer is created on this website

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,5 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layout/Main";
+import Home from "../Pages/Home/Home/Home";
 
 
 export const router = createBrowserRouter([
@@ -8,7 +9,8 @@ export const router = createBrowserRouter([
     element: <Main></Main>,
     children: [
       {
-        
+        path: '/',
+        element: <Home></Home>
       }
     ]
   },
