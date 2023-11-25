53c2e20258bf14437d9502d84cffd1a7bc59e9f7
Tawkir711 <mdtawkirk711@gmail.com>
2023-11-25 19:55:08 +0600

    added firebase config

---
+++ src/Component/Firebase/Firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAYAqwYPwD2K5RiuQrI4TALm4bC3dxZxJo",
+  authDomain: "medical-camp-1e7f0.firebaseapp.com",
+  projectId: "medical-camp-1e7f0",
+  storageBucket: "medical-camp-1e7f0.appspot.com",
+  messagingSenderId: "53530079177",
+  appId: "1:53530079177:web:2791b884c9950e4adc8218"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
