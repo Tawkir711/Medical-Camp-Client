b4bffc10a5fa6be4fe11620cb01ec3d0adad4d85
Tawkir711 <mdtawkirk711@gmail.com>
2023-11-25 19:47:17 +0600

    Error Page has been created on this website

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -4,12 +4,14 @@ import Home from "../Pages/Home/Home/Home";
 import Registration from "../Pages/Home/UpcomingCamps/Registration";
 import AvailableCamps from "../Pages/AvailableCamps/AvailableCamps";
 import ContactUs from "../Pages/ContactUs/Contact";
+import ErrorPage from "../Pages/ErrorPage/ErrorPage";
 
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <Main></Main>,
+    errorElement:<ErrorPage></ErrorPage>,
     children: [
       {
         path: "/",
