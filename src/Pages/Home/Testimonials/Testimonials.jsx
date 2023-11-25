68217a7496e63873195160bd137fadba0c06851f
Tawkir711 <mdtawkirk711@gmail.com>
2023-11-25 10:43:58 +0600

    This website Testimonials added

---
+++ src/Pages/Home/Testimonials/Testimonials.jsx
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from "react";
+import { Navigation, Pagination, Mousewheel, Keyboard } from "swiper/modules";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+import { Rating } from "@smastrom/react-rating";
+import "@smastrom/react-rating/style.css";
+
+const Testimonials = () => {
+  const [reviews, setReviews] = useState([]);
+  useEffect(() => {
+    fetch("testimonials.json")
+      .then((res) => res.json())
+      .then((data) => setReviews(data));
+  }, []);
+  return (
+    <section className="bg-base-100 my-4">
+      <Swiper
+        cssMode={true}
+        navigation={true}
+        pagination={true}
+        mousewheel={true}
+        keyboard={true}
+        modules={[Navigation, Pagination, Mousewheel, Keyboard]}
+        className="mySwiper"
+      >
+        {reviews.map((review) => (
+          <SwiperSlide key={review._id}>
+            <div className="flex flex-col items-center mx-24 my-12">
+              <h2 className="text-2xl text-orange-400">{review.name}</h2>
+              <h2 className="text-2xl text-orange-400 mt-3">{review.date}</h2>
+              <p className="py-8 font-medium">{review.details}</p>
+              <Rating
+                style={{ maxWidth: 180 }}
+                value={review.rating}
+                readOnly
+              />
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </section>
+  );
+};
+
+export default Testimonials;
