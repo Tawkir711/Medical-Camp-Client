cfadafbb79773f3e9cf61a2cfaff08f4f13cba89
Tawkir711 <mdtawkirk711@gmail.com>
2023-11-28 16:59:02 +0600

    added join camp modal and data send to database

--- src/Pages/Home/PopularCamps/DetailsPage.jsx
+++ src/Pages/Home/PopularCamps/DetailsPage.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Link, useLoaderData } from 'react-router-dom';
+import React from "react";
+import { Link, useLoaderData } from "react-router-dom";
 import { IoLocationSharp } from "react-icons/io5";
 import { LiaCampgroundSolid } from "react-icons/lia";
 import { BsCalendar2DateFill } from "react-icons/bs";
@@ -7,10 +7,44 @@ import { HiMiniCurrencyDollar } from "react-icons/hi2";
 import { MdMedicalServices } from "react-icons/md";
 import { GrUserManager } from "react-icons/gr";
 import { FaUsers } from "react-icons/fa";
+import { useForm } from "react-hook-form";
+import Swal from "sweetalert2";
 
 const DetailsPage = () => {
-  const { _id, name, image, fees, date, location, service, health, audience } =  useLoaderData();
-
+  const { _id, name, image, fees, date, location, service, health, audience } =
+    useLoaderData();
+  const { register, handleSubmit,reset } = useForm();
+  const onSubmit = async (data) => {
+    console.log(data);
+    const campItem = {
+      name: data.name,
+      age: data.age,
+      phone: data.phone,
+      address: data.address,
+      gender: data.gender,
+      contact: data.contact,
+      requirement: data.requirement
+    }
+    fetch('http://localhost:5000/joinCamp', {
+      method: "POST",
+      headers: {
+        "content-type": "application/json"
+      },
+      body: JSON.stringify(campItem)
+    })
+      .then(res => res.json())
+      .then(data => {
+        console.log(data)
+        if (data.insertedId) {
+          reset();
+          Swal.fire({
+            icon: "success",
+            title: "Done",
+            text: "Join Camp Added Successfully"
+          });
+        }
+    })
+  };
   return (
     <div className="card bg-base-100 shadow-xl p-5">
       <h2 className="text-3xl font-semibold text-blue-600 text-center pb-3">
@@ -49,9 +83,113 @@ const DetailsPage = () => {
           <FaUsers className="mr-2 text-2xl" /> {audience}
         </p>
       </div>
-      <Link to={''}>
-        <button id='my_modal_5' className="btn btn-primary text-white w-1/2 ">Join Camp</button>
-      </Link>
+      {/* Open the modal using document.getElementById('ID').showModal() method */}
+      <button
+        className="btn btn-primary text-white"
+        onClick={() => document.getElementById("my_modal_5").showModal()}
+      >
+        Join Camp
+      </button>
+      <dialog id="my_modal_5" className="modal modal-bottom sm:modal-middle">
+        <div className="modal-box">
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div className="flex gap-2">
+              <div className="form-control md:w-1/2">
+                <label className="label">
+                  <span className="label-text">Name</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="Name"
+                  {...register("name", { required: true })}
+                  className="input input-bordered w-full"
+                />
+              </div>
+              <div className="form-control md:w-1/2">
+                <label className="label">
+                  <span className="label-text">Age</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="Age"
+                  {...register("age", { required: true })}
+                  className="input input-bordered w-full"
+                />
+              </div>
+            </div>
+            <div className="flex gap-2">
+              <div className="form-control md:w-1/2">
+                <label className="label">
+                  <span className="label-text">Phone</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="Phone"
+                  {...register("phone", { required: true })}
+                  className="input input-bordered w-full"
+                />
+              </div>
+              <div className="form-control md:w-1/2">
+                <label className="label">
+                  <span className="label-text">Address</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="Address"
+                  {...register("address", { required: true })}
+                  className="input input-bordered w-full"
+                />
+              </div>
+            </div>
+            <div className="form-control w-full">
+              <label className="label">
+                <span className="label-text">Gender</span>
+              </label>
+              <select
+                {...register("gender")}
+                className="select select-bordered w-full "
+              >
+                <option disabled value={"default"}>
+                  Select a category
+                </option>
+                <option value={"male"}>Male</option>
+                <option value={"female"}>Female</option>
+              </select>
+            </div>
+            <div className="flex gap-2">
+              <div className="form-control md:w-1/2">
+                <label className="label">
+                  <span className="label-text">Emergency Contact</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="Emergency Contact"
+                  {...register("contact", { required: true })}
+                  className="input input-bordered w-full"
+                />
+              </div>
+              <div className="form-control md:w-1/2">
+                <label className="label">
+                  <span className="label-text">Requirement</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="Requirement"
+                  {...register("requirement", { required: true })}
+                  className="input input-bordered w-full"
+                />
+              </div>
+            </div>
+            <button className="btn btn-primary text-white border-0 w-full hover:bg-red-400 my-2">
+              Join Camp
+            </button>
+            <form method="dialog" className="flex justify-center">
+              {/* if there is a button in form, it will close the modal */}
+              <button className="btn btn-primary text-white">Close</button>
+            </form>
+          </form>
+        </div>
+      </dialog>
     </div>
   );
 };
